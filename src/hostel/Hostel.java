338ce37b662548662a27a7a1aac609a2fd4bce9a
Rosendark <remirosenblatt@gmail.com>
2020-01-17 14:36:10 +0100

    Class Hostel OK

--- src/hostel/Hostel.java
+++ src/hostel/Hostel.java
@@ -1,4 +1,40 @@
 package hostel;
 
+import java.util.ArrayList;
+
 public class Hostel {
+
+    protected ArrayList<Chamber> chambers;
+    protected ArrayList<Client> clients;
+    protected ArrayList<Reservation> reservations;
+
+    public Hostel(ArrayList<Chamber> chambers, ArrayList<Client> clients, ArrayList<Reservation> reservations) {
+        this.chambers = chambers;
+        this.clients = clients;
+        this.reservations = reservations;
+    }
+
+    public ArrayList<Chamber> getChambers() {
+        return chambers;
+    }
+
+    public void setChambers(ArrayList<Chamber> chambers) {
+        this.chambers = chambers;
+    }
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<Client> clients) {
+        this.clients = clients;
+    }
+
+    public ArrayList<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(ArrayList<Reservation> reservations) {
+        this.reservations = reservations;
+    }
 }
