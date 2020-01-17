0f138abc15c726d46e18fc03a6e945df6ed37b0e
Rosendark <remirosenblatt@gmail.com>
2020-01-17 14:22:57 +0100

    Class Client OK

--- src/hostel/Client.java
+++ src/hostel/Client.java
@@ -1,4 +1,40 @@
 package hostel;
 
+import java.util.ArrayList;
+
 public class Client {
+
+    protected String nom;
+    protected String email;
+    protected ArrayList<Reservation> reservations;
+
+    public Client(String nom, String email, ArrayList<Reservation> reservations) {
+        this.nom = nom;
+        this.email = email;
+        this.reservations = new ArrayList<Reservation>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(ArrayList<Reservation> reservations) {
+        this.reservations = reservations;
+    }
 }
