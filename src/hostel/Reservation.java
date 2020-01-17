006ff2059e80435bd0a8f3313ba2d199e29a70b0
Pierrick <pierrickgirardot@gmail.com>
2020-01-17 14:26:32 +0100

    Début de réservation

--- src/hostel/Reservation.java
+++ src/hostel/Reservation.java
@@ -1,4 +1,21 @@
 package hostel;
 
+import java.util.Date;
+
 public class Reservation {
+    private int numero;
+    private Date dateArrivee;
+    private Date dateDepart;
+    private int nbPersonnes;
+    private boolean aEnfant;
+    private Chamber chamber;
+
+    public Reservation(int numero, Date dateArrivee, Date dateDepart, int nbPersonnes, boolean aEnfant) {
+        this.numero = numero;
+        this.dateArrivee = dateArrivee;
+        this.dateDepart = dateDepart;
+        this.nbPersonnes = nbPersonnes;
+        chamber = null;
+    }
+
 }
---
+++ src/hostel/dateCreation.java
@@ -0,0 +1,15 @@
+package hostel;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class dateCreation {
+    private static Date createDate(int jour, int mois, int annee) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, jour);
+        //Ah oui, c'est vrai, les mois vont de 0 à 11
+        calendar.set(Calendar.MONTH, mois - 1);
+        calendar.set(Calendar.YEAR, annee);
+        return calendar.getTime();
+    }
+}
