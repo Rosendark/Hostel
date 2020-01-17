5fa0c8ffa484a9a938f5ce582b6f6d7132b6ea32
Rosendark <remirosenblatt@gmail.com>
2020-01-17 14:17:11 +0100

    Class Chamber ok

--- src/Chamber.java
+++ src/Chamber.java
@@ -1,2 +1,36 @@
 public class Chamber {
+
+    protected int numero;
+    protected int capacite;
+    protected  CATEGORIE categorie;
+
+    public Chamber(int numero, int capacite, CATEGORIE categorie) {
+        this.numero = numero;
+        this.capacite = capacite;
+        this.categorie = categorie;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCapacite() {
+        return capacite;
+    }
+
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+
+    public CATEGORIE getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(CATEGORIE categorie) {
+        this.categorie = categorie;
+    }
 }
